3939f56a72a64609c40426dff669825da36eb746
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-04-03 18:42:29 -0400

    Working on the routes, not working yet

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,14 @@
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+
+app.get("/survey", function(req, res) {
+    res.json(friendsData);
+  });
+
+app.post("/api/survey", function(req, res) {
+    friendsData.push(req.body);
+      res.json(true);
+});
+
+};
