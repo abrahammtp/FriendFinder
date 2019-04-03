ff63b3b257755491ca55d10fabcc356bc99231c6
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-04-03 19:15:57 -0400

    Routes are working now

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,14 @@
 var friendsData = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-app.get("/survey", function(req, res) {
-    res.json(friendsData);
-  });
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+      });
 
-app.post("/api/survey", function(req, res) {
-    friendsData.push(req.body);
-      res.json(true);
-});
+    app.post("/api/survey", function (req, res) {
+        friendsData.push(req.body);
+        res.json(true);
+    });
 
 };
