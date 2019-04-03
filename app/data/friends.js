c720b1e609991f41a4ee35e52f38ffd8f5be1231
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-04-03 17:23:36 -0400

    Added friends to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+    {
+        name: "Sarah",
+        photo: "https://pbs.twimg.com/profile_images/1053361269313622016/Aw0gEqQR_400x400.jpg",
+        scores: [
+            5,
+            2,
+            3,
+            4,
+            1,
+            1,
+            3,
+            4,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Michael",
+        photo: "https://m.media-amazon.com/images/M/MV5BY2Q5YmMyNzAtMmYyZi00YTgzLTgxM2EtZWM3OThlMjcyNjQxXkEyXkFqcGdeQXVyNzk5ODE2NDA@._V1_.jpg",
+        scores: [
+            2,
+            3,
+            5,
+            1,
+            3,
+            4,
+            2,
+            1,
+            1,
+            3
+        ]
+    },
+    {
+        name: "Jennifer",
+        photo: "https://pixel.nymag.com/imgs/fashion/daily/2019/02/26/26-jennifer-lawrence-lede.w330.h330.jpg",
+        scores: [
+            4,
+            3,
+            1,
+            2,
+            5,
+            4,
+            2,
+            1,
+            3,
+            4
+        ]
+    },
+];
+
+module.exports = friends;
