0237826e9204b0c5f498c939e175fac4fe41fab0
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-04-04 17:07:51 -0400

    Functionality of the app is done, now I need to work on the CSS

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,26 @@ module.exports = function (app) {
       });
 
     app.post("/api/friends", function (req, res) {
+        var match;
+        var newFriend = req.body;
+        var scoreDifference = 1000;
+
+        for (let i = 0; i < friendsData.length; i++) {
+            var newScoreDifference = 0;
+
+            for (let h = 0; h < friendsData[i].scores.length; h++) {
+                newScoreDifference += parseInt(Math.abs(newFriend.scores[h]) - parseInt(friendsData[i].scores[h]));
+            }
+
+            if (newScoreDifference < scoreDifference) {
+                scoreDifference = newScoreDifference
+                match = friendsData[i];
+                console.log(match)
+            }
+        }
+
         friendsData.push(req.body);
-        res.json(true);
+        res.json(match);
     });
 
 };
